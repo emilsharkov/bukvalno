aa15b1b4cadcbe6d75cd5f31c5cce850e74e6c82
Emil Sharkov <emosharkov@gmail.com>
2024-12-28 09:59:03 -0500

    formatting

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use bukvalno::{ascii::generate_ascii_art, cli::Config};
+use bukvalno::{ascii, cli};
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let config = Config::parse_args_to_config()?;
-    let ascii_art = generate_ascii_art(&config)?;
+    let config = cli::Config::parse_args_to_config()?;
+    let ascii_art = ascii::generate_ascii_art(&config)?;
     print!("{}", ascii_art);
     Ok(())
 }
